125d68f0d5b037b9a165af833afe05fcf79172b0
webenia <ahmedhusisn771@yahoo.com>
2025-04-15 13:00:46 +0200

    ,

--- app/src/main/java/com/webenia/eleganceoud/presentation/screens/signup/SignUpScreen.kt
+++ app/src/main/java/com/webenia/eleganceoud/presentation/screens/signup/SignUpScreen.kt
@@ -11,23 +11,35 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxColors
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomEnd
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.End
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
@@ -65,7 +77,8 @@ fun SignUpScreenContent(
     val keyboardController = LocalSoftwareKeyboardController.current
 
     Column(
-        modifier = Modifier.verticalScroll(rememberScrollState())
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
             .padding(20.dp)
             .fillMaxSize(),
         verticalArrangement = Arrangement.spacedBy(10.dp),
@@ -93,7 +106,7 @@ fun SignUpScreenContent(
             contentDescription = "Logo",
             modifier = Modifier
                 .align(CenterHorizontally)
-                .size(width = 350.dp, height = 200.dp)
+                .size(width = 300.dp, height = 150.dp)
         )
         Spacer(modifier = Modifier.height(8.dp))
         Text(
@@ -101,7 +114,7 @@ fun SignUpScreenContent(
             color = MidGrey,
             fontFamily = poppinsFamily,
             fontWeight = FontWeight.Bold,
-            fontSize = 24.sp
+            fontSize = 22.sp
         )
         Spacer(modifier = Modifier.height(6.dp))
         Text(
@@ -154,13 +167,57 @@ fun SignUpScreenContent(
             imeAction = ImeAction.Done,
             focusRequester = focusRequesterConfirmPassword
         )
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.End
+        ) {
+            Text(
+                text = "Already have an account?",
+                color = Primary,
+                fontFamily = poppinsFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(modifier = Modifier.fillMaxWidth()) {
+            var checkState by remember { mutableStateOf(false) }
+            Checkbox(
+                checked = checkState,
+                onCheckedChange = {checkState = it},
+                colors = CheckboxDefaults.colors(checkedColor = Primary)
+            )
+            Text(
+                text = stringResource(R.string.agree_signup),
+                color = LightGrey,
+                fontFamily = poppinsFamily,
+                fontWeight = FontWeight.Normal,
+                fontSize = 14.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        Button(modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .height(50.dp), shape = RoundedCornerShape(20.dp),
+            colors = ButtonDefaults.buttonColors(containerColor = Primary), onClick = {}) {
+            Text(
+                text = "Sign up",
+                color = Color.White,
+                fontFamily = poppinsFamily,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
+            )
+        }
 
     }
 
 }
 
 @Composable
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 fun SignUpScreenContentPreview() {
     SignUpScreenContent(state = SignUpUiState(), onEvent = {})
 }
